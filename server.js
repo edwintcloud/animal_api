9dc9b66bd35ba61fc5919e03e32bcb5463f1cd80
Edwin Cloud <ecloud412@gmail.com>
2018-10-29 16:40:26 -0700

    :zap: Finished first two get routes

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 const app = require('express')();
+const router = require('express')(Router);
+
+// set up express for json response
+app.use(express.json());
 
 // zoo collection
-const zoo = [];
+const zoos = [];
+
+// animal collection
+const animals = [];
+
+// zoo model
+const zoo = {
+  id: 0,
+  name: ""
+}
 
 // animal model
 const animal = {
-  id: 0
+  id: 0,
   name: "",
   type: ""
 };
 
-// routes
-app.get('/', (req, res) => {
-
+// gets all zoos
+router.get('/zoos', (req, res) => {
+  res.json(zoos);
 });
+
+// gets a single zoo
+router.get('/zoos/:id', (req, res) => {
+  try {
+    res.json(zoo[req.params.id]);
+  } catch (e) {
+    res.json({
+      "message": e
+    });
+  }
+});
+
+/*
+  
+*/
+
+// use our routes
+app.use('/api', router)
 
 // start app
 app.listen('5000', () => {
